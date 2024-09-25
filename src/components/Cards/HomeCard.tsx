7fad80af3a318c839c88e48692dc9d4e9912e7bd
divyesh123-jain <divyeshjain318@gmail.com>
2024-09-25 09:36:29 +0530

    added stats

--- src/components/Cards/HomeCard.tsx
+++ src/components/Cards/HomeCard.tsx
@@ -6,14 +6,7 @@ export default function HomeCard() {
         <div className="relative bg-green-500 rounded-lg  p-6 w-full max-w-4xl mx-auto shadow-[6px_7px_#1536c6] cursor-pointer my-[20px]   border-2 border-solid active:shadow-none active:translate-x-[3px] active:translate-y-[3px] ">
 
   <div className="relative z-10 flex justify-between items-center text-white mb-4">
-    <div className="flex items-center space-x-2">
     
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-        <path d="M10 2a6 6 0 00-6 6v3.586L3.707 14.707a1 1 0 001.414 0L7 12.828V8a5 5 0 0110 0v4.828l1.879 1.879a1 1 0 001.414-1.414L16 11.586V8a6 6 0 00-6-6z" />
-      </svg>
-    
-      <span>08 12 21</span>
-    </div>
 
     <div className="flex items-center space-x-4">
       {/* <img src="team1.png" alt="Team 1" className="h-10 w-10 rounded-full">
@@ -32,26 +25,26 @@ export default function HomeCard() {
   <div className="relative z-10 flex justify-between items-center mb-8 gap-4 md:gap-0">
     <div className="text-white text-center shadow-md rounded-lg p-4 bg-green-600">
       <div className="text-lg font-bold">Total Matches</div>
-      <div className="text-sm">West Ham United</div>
+      <div className="text-sm"> 500+</div>
     </div>
    
     <div className="text-white text-center shadow-md rounded-lg p-4 bg-green-600">
       <div className="text-lg font-bold">Total Runs</div>
-      <div className="text-sm">Bayer Leverkusen</div>
+      <div className="text-sm">25,000+</div>
     </div>
   </div>
 
   <div className="relative z-10 flex justify-between items-center text-white  md:gap-0 gap-4 text-sm md:text-base">
     <div className="text-center shadow-md rounded-lg p-4 bg-green-600">
-      <div>WHU 44% TIP</div>
-      <div>234 TIP</div>
+      <div>Batting Average</div>
+      <div>53+</div>
     </div>
-    <div className="text-center mt-8 shadow-md rounded-lg p-2  bg-green-600">
+    <div className="text-center mt-8 shadow-xl text-2xl font-bold rounded-lg p-2  ">
     <div>Virat Kholi </div>
     </div>
     <div className="text-center shadow-md rounded-lg p-4 bg-green-600">
-      <div>LEV 32% TIP</div>
-      <div>131 TIP</div>
+      <div>Recent Form TIP</div>
+      <div>44% TIP</div>
     </div>
   </div>
   {/* <div class="absolute left-1/2 inset-0 z-0 transform -translate-x-1/2 -top-3">
